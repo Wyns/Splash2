05858b68a2e959539f299f1a2b78c471327c834b
Wyns <pauwains@gmail.com>
2015-07-26 13:10:36 +0300

    added SplashScreen.java

---
+++ app/src/main/java/com/example/paul/splash2/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.paul.splash2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by paul on 26/07/15.
+ */
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
